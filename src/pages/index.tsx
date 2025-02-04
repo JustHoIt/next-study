f9cf55d901710f85c60c4c6820fedfbdc18c9d8a
JustHo <zalop3128@kakao.com>
2025-02-04 23:05:27 +0900

    routing

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,20 @@
 export default function Home() {
   return (
-   <main>HOME</main>
+   <main>
+    <ul>
+      <li>
+        <a href="/routing">STATIC</a>
+      </li>
+      <li>
+        <a href="/routing/dynamic/sadwssad">DYNAMICC</a>
+      </li>
+      <li>
+        <a href="/routing/nested/asdasd/routing">NESTED</a>
+      </li>
+      <li>
+        <a href="/routing/catch-all-segments/1/2/3/4/5/6/a/d/s">CATCH-ALL-SEGMENTS</a>
+      </li>
+    </ul>
+   </main>
   )
 }
---
+++ src/pages/routing/nested/[any]/routing/index.tsx
@@ -0,0 +1,8 @@
+import { FC } from "react";
+
+const NestedRouting: FC = () => {
+  return <main>NESTED-ROUTING</main>
+};
+
+
+export default NestedRouting;
