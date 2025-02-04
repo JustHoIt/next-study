e47ae7e7144e8583b426861515437b0c9f4e62c8
JustHo <zalop3128@kakao.com>
2025-02-05 03:36:07 +0900

    Font Local

---
+++ src/domains/next-functions/font/google/index.tsx
@@ -0,0 +1,9 @@
+import { FC } from "react";
+
+
+const GoogleMain: FC = () => {
+  return <main>GOOGLE</main>
+};
+
+
+export { GoogleMain };
---
+++ src/pages/next-functions/font/local/index.tsx
@@ -0,0 +1,11 @@
+import { LocalMain } from "@/domains/next-functions/font/local";
+import { FC } from "react";
+
+
+const LocalPage: FC = () => {
+
+  return <LocalMain />
+
+};
+
+export default LocalPage;
